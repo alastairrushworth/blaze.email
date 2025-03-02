b2f0b24207200cb7f59f54ab434823f866b10fbd
alastair rushworth <alastairmrushworth@gmail.com>
2025-03-02 15:17:17 +0000

    update about

--- site/app/about/page.tsx
+++ site/app/about/page.tsx
@@ -6,16 +6,15 @@ export default function AboutPage() {
       <h1 className="text-5xl font-bold mb-8 text-center text-indigo-800 dark:text-indigo-200">About blaze.email</h1>
       <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-8 mb-8">
         <p className="text-gray-700 dark:text-gray-300 mb-4">
-          Welcome to blaze.email, your premier source for curated weekly tech newsletters. We're passionate about
-          keeping you informed on the latest trends, innovations, and insights across various technology domains.
-        </p>
-        <p className="text-gray-700 dark:text-gray-300 mb-4">
-          At blaze.email, we believe in the power of knowledge and the importance of staying ahead in the fast-paced
-          world of technology. Our mission is to empower you with the information you need to excel in your field and
+          Blaze is an email newsletter service that automatically curates the best independent technical writing from
+          the past week into newsletter digests. We place a strong focus on discovery, and try to strike a balance between
+          quality, relevance and diversity.
+
+          The project was borne out of frustration with the traditional means of discovering and reading technical content <a href='https://alastairrushworth.com/posts/why-finding-good-tech-blogs-is-hard/' className="text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-200 transition-colors">'Why finding good tech blogs is hard'</a>.
+
         </p>
         <p className="text-gray-700 dark:text-gray-300">
-          Join thousands of tech professionals who trust blaze.email for their weekly dose of curated tech insights.
-          Choose from our range of specialized newsletters and ignite your tech knowledge today!
+          I'd really appreciate any feedback you have on blaze or ways it might be improved - you can either drop me a line directly or add some <a href='https://95nowtw3un3.typeform.com/to/prerGYiP' className="text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-200 transition-colors">feedback using this anonymous typeform.</a>
         </p>
       </div>
       <div className="text-center">
@@ -23,7 +22,7 @@ export default function AboutPage() {
           href="/"
           className="text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-200 transition-colors"
         >
-          ← Back to Home
+          ← Back home
         </Link>
       </div>
     </div>
