cf172718ccae07323041d459fbff5bb3ec341efb
alastair rushworth <alastairmrushworth@gmail.com>
2025-05-09 07:36:58 +0100

    https on w3

--- site/app/[topic]/page.tsx
+++ site/app/[topic]/page.tsx
@@ -184,7 +184,7 @@ export default async function TopicPage({ params }: { params: { topic: string } 
                   >
                     <div className="flex items-center">
                       <span className="text-indigo-600 dark:text-indigo-400 mr-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="https://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg>
                       </span>
